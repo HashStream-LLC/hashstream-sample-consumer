dc90f779c999469efe0cc39031cf333c5e698031
Chad Schwab <chad.schwab@lworks.io>
2025-03-13 08:30:07 -0500

    remove duplicate annotation

--- hashstream-consumer-java/src/main/java/xyz/hashstream/consumer/lambda/controller/WebhookConsumerController.java
+++ hashstream-consumer-java/src/main/java/xyz/hashstream/consumer/lambda/controller/WebhookConsumerController.java
@@ -6,13 +6,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import xyz.hashstream.consumer.lambda.model.NotificationPayload;
 import xyz.hashstream.consumer.lambda.model.WebhookResponse;
 
 @RestController
-@EnableWebMvc
 public class WebhookConsumerController {
   Logger logger = LoggerFactory.getLogger(WebhookConsumerController.class);
 
